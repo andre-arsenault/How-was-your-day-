fa8f332be475e1e78fb2713fc2c072edd4e4c8c3
Andre Arsenault <andre@snowleopardgames.com>
2014-01-25 22:06:47 +0100

    Revert "Merge branch 'master' of https://github.com/andre-arsenault/How-was-your-day-"
    This reverts commit b1e24034117d38cb9770568ca68fcd3a0d53c1d2, reversing
    changes made to 551db994101718e9919de43d9f7d69f240f6c7a4.

--- Assets/Standard Assets/Scripts/Dialogue/Score.js
+++ Assets/Standard Assets/Scripts/Dialogue/Score.js
@@ -3,11 +3,6 @@
 
 static var good_endings = new Hashtable();
 
-good_endings['Divorce'] = null;
 good_endings['Domestic'] = null;
-<<<<<<< HEAD
 good_endings['Loneliness'] = null;
 good_endings['Divorce'] = null;
-=======
-good_endings['Loneliness'] = null;
->>>>>>> d1f0eae3d960e7f6562ef92775bf4b8f25cfd3e9
