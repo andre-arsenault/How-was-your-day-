25210f83f6e6fc360d061f2545992798f9fd5002
TrapperHell <trapper-hell@hotmail.com>
2014-01-25 22:34:16 +0100

    Included Credits, train toy, and train images.

---
+++ Assets/Scripts/CreditsController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreditsController : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        float pixelRatio = (Camera.main.orthographicSize * 2) / Camera.main.pixelHeight;
+        transform.localScale = new Vector3(pixelRatio * 10f, pixelRatio * 10f, pixelRatio * 10f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
